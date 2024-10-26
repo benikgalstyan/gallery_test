0181ad933284bfbecb572f93add6cfaaad72827e
benik <galstyan.benik@gmail.com>
2024-10-26 13:23:06 +0300

    fetch photos

--- lib/data/repository/repository.dart
+++ lib/data/repository/repository.dart
@@ -1,1 +1,9 @@
-abstract interface class Repository {}
+import 'package:gallery_test/data/models/photo.dart';
+
+abstract interface class Repository {
+  Future<List<Photo>> fetchPhotos(int page, int perPage);
+
+  Future<void> cachePhotos(List<Photo> photos, int page);
+
+  Future<List<Photo>> getCachedPhotosAsPhotos(int page, int perPage);
+}
