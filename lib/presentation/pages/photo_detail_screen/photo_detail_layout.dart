a8653f5775bafd9fa455db9ae843853b2babb4ea
benik <galstyan.benik@gmail.com>
2024-10-26 13:22:33 +0300

    implemented photo detail screen

--- lib/presentation/pages/photo_detail_screen/photo_detail_layout.dart
+++ lib/presentation/pages/photo_detail_screen/photo_detail_layout.dart
@@ -1,15 +1,55 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:gallery_test/core/theme/text_styles.dart';
+import 'package:gallery_test/data/models/photo.dart';
+import 'package:gallery_test/presentation/tokens/spacing.dart';
 
 class PhotoDetailLayout extends StatefulWidget {
-  const PhotoDetailLayout({super.key});
+  const PhotoDetailLayout({super.key, required this.photo});
+
+  final Photo photo;
 
   @override
   State<PhotoDetailLayout> createState() => _PhotoDetailLayoutState();
 }
 
+const iconSize = 64.0;
+
 class _PhotoDetailLayoutState extends State<PhotoDetailLayout> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text(widget.photo.author)),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CachedNetworkImage(
+                imageUrl: widget.photo.url,
+                fit: BoxFit.cover,
+                placeholder: (context, url) =>
+                    const CircularProgressIndicator(),
+                errorWidget: (context, url, error) => const Icon(
+                  Icons.error,
+                  size: iconSize,
+                ),
+              ),
+              Spacings.spacer16,
+              Padding(
+                padding: Spacings.paddingH16,
+                child: Text(
+                  widget.photo.description,
+                  textAlign: TextAlign.center,
+                  style: TextStyles.secondaryTextStyle,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/presentation/pages/photo_detail_screen/photo_detail_screen.dart
+++ lib/presentation/pages/photo_detail_screen/photo_detail_screen.dart
@@ -1,15 +1,20 @@
 import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
+import 'package:gallery_test/data/models/photo.dart';
 import 'package:gallery_test/presentation/pages/photo_detail_screen/photo_detail_layout.dart';
 
 @RoutePage()
 class PhotoDetailScreen extends StatelessWidget {
-  const PhotoDetailScreen({super.key});
+  const PhotoDetailScreen({super.key, required this.photo});
+
+  final Photo photo;
 
   static const nameRoute = '/PhotoDetailScreen';
 
   @override
   Widget build(BuildContext context) {
-    return const PhotoDetailLayout();
+    return PhotoDetailLayout(
+      photo: photo,
+    );
   }
 }
