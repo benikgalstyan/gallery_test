620007a72a5da349d8e382a1b545ea36cefe7cd9
benik <galstyan.benik@gmail.com>
2024-10-26 13:22:20 +0300

    implemented main bloc

--- lib/presentation/pages/main_screen/bloc/main_bloc.dart
+++ lib/presentation/pages/main_screen/bloc/main_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gallery_test/data/models/photo.dart';
 import 'package:gallery_test/data/repository/repository.dart';
 
 part 'main_event.dart';
@@ -7,7 +8,33 @@ part 'main_event.dart';
 part 'main_state.dart';
 
 class MainBloc extends Bloc<MainEvent, MainState> {
-  MainBloc(this.repository) : super(MainInitialState());
+  MainBloc(this.repository) : super(MainInitialState()) {
+    on<FetchPhotosEvent>(_onFetchPhotos);
+  }
 
   final Repository repository;
+
+  Future<void> _onFetchPhotos(
+    FetchPhotosEvent event,
+    Emitter<MainState> emit,
+  ) async {
+    emit(MainLoadingState());
+
+    try {
+      final List<Photo> photos =
+          await repository.fetchPhotos(event.page, event.perPage);
+
+      await repository.cachePhotos(photos, event.page);
+
+      emit(MainLoadedState(photos: photos));
+    } catch (e) {
+      final cachedPhotos =
+          await repository.getCachedPhotosAsPhotos(event.page, event.perPage);
+      if (cachedPhotos.isNotEmpty) {
+        emit(MainLoadedState(photos: cachedPhotos));
+      } else {
+        emit(MainErrorState('Error: ${e.toString()}'));
+      }
+    }
+  }
 }
--- lib/presentation/pages/main_screen/bloc/main_state.dart
+++ lib/presentation/pages/main_screen/bloc/main_state.dart
@@ -7,7 +7,11 @@ final class MainInitialState extends MainState {}
 
 final class MainLoadingState extends MainState {}
 
-final class MainLoadedState extends MainState {}
+final class MainLoadedState extends MainState {
+  MainLoadedState({required this.photos});
+
+  final List<Photo> photos;
+}
 
 final class MainErrorState extends MainState {
   MainErrorState(this.exception);
